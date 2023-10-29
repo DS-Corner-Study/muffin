84d887412cbc58c2b43758e9fc304b8c964a8889
mk020 <mk02081@naver.com>
2023-10-29 20:03:36 +0900

    logging

--- SpringBoot/api/src/main/java/com/springboot/api/controller/GetController.java
+++ SpringBoot/api/src/main/java/com/springboot/api/controller/GetController.java
@@ -3,17 +3,25 @@ package com.springboot.api.controller;
 import com.springboot.api.dto.MemberDto;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
+import java.util.logging.Logger;
 
+@Slf4j
 @RestController
 @RequestMapping("/api/v1/get-api")
 public class GetController {
 
+//    private final Logger LOGGER = (Logger) LoggerFactory.getLogger(GetController.class);
+
     // http://localhost:8080/api/v1/get-api/hello
     @RequestMapping(value = "/hello", method = RequestMethod.GET)
     public String getHello() {
+//        LOGGER.info("getHello 메서드가 호출되었습니다.");
+        log.info("getHello 메서드가 호출되었습니다.");
         return "Hello World";
     }
 
@@ -26,6 +34,7 @@ public class GetController {
     // http://localhost:8080/api/v1/get-api/variable1/{String 값}
     @GetMapping(value = "/variable1/{variable}")
     public String getVariable1(@PathVariable String variable) {
+        log.info("@PathVariable을 통해 들어온 값: {}", variable);
         return variable;
     }
 
