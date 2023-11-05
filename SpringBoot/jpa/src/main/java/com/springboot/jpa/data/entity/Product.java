942335f19cd7b6a6ed895bc6ce048f1d76aed4d7
mk020 <mk02081@naver.com>
2023-11-05 22:05:25 +0900

    Lombok 어노테이션

--- SpringBoot/jpa/src/main/java/com/springboot/jpa/data/entity/Product.java
+++ SpringBoot/jpa/src/main/java/com/springboot/jpa/data/entity/Product.java
@@ -1,14 +1,18 @@
 package com.springboot.jpa.data.entity;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
 
+@Entity
 @Getter
 @Setter
-@Entity
+@NoArgsConstructor
+@AllArgsConstructor
 @Table(name = "product")
 public class Product {
 
