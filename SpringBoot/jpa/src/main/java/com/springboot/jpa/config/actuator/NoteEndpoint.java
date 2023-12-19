6c672efaa3d7a078d49ab876dd1f353659e8d979
mk020 <mk02081@naver.com>
2023-12-19 15:53:00 +0900

    액추에이터 활용하기

---
+++ SpringBoot/jpa/src/main/java/com/springboot/jpa/config/actuator/NoteEndpoint.java
@@ -0,0 +1,32 @@
+package com.springboot.jpa.config.actuator;
+
+import org.springframework.stereotype.Component;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Component
+// 커스텀 엔드포인트 생성
+//@Endpoint(id = "note", enableByDefault = true)
+public class NoteEndpoint {
+
+//    private Map<String, Object> noteContent = new HashMap<>();
+//
+//    @ReadOperation
+//    public Map<String, Object> getNote(){
+//        return noteContent;
+//    }
+//
+//    @WriteOperation
+//    public Map<String, Object> writeNote(String key, Object value){
+//        noteContent.put(key,value);
+//        return noteContent;
+//    }
+//
+//    @DeleteOperation
+//    public Map<String, Object> deleteNote(String key){
+//        noteContent.remove(key);
+//        return noteContent;
+//    }
+
+}
